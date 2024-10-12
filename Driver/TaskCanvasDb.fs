291752dfd1e436cd7b23e1e633fcd39663f3bef0
kamegoro <tentyo0620@icloud.com>
2024-10-12 17:41:59 +0900

    add: select tag driver

---
+++ Driver/TaskCanvasDb.fs
@@ -0,0 +1,22 @@
+namespace task_canvas_tag_manager.Driver
+
+open Dapper.FSharp.PostgreSQL
+open System.Data
+
+module TaskCanvasDb =
+    type Tag = { id: string; name: string; }
+
+    let tagTable = table'<Tag> "tag" |> inSchema "task_canvas"
+
+    let selectTags (conn: IDbConnection) : Async<Tag list> =
+        async {
+            let! queryResult =
+                select {
+                    for t in tagTable do
+                    selectAll
+                }
+                |> conn.SelectAsync<Tag>
+                |> Async.AwaitTask
+
+            return queryResult |> List.ofSeq
+        }
