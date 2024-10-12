102bf6fefc3b82393646470e833803059f930b2d
kamegoro <tentyo0620@icloud.com>
2024-10-13 01:34:21 +0900

    fix id type

--- TaskCanvasManager/Driver/TaskCanvasDb.fs
+++ TaskCanvasManager/Driver/TaskCanvasDb.fs
@@ -2,9 +2,10 @@ namespace task_canvas_tag_manager.Driver
 
 open Dapper.FSharp.PostgreSQL
 open System.Data
+open System
 
 module TaskCanvasDb =
-    type Tag = { id: string; name: string }
+    type Tag = { id: Guid; name: string }
 
     let tagTable = table'<Tag> "tag" |> inSchema "task_canvas"
 
