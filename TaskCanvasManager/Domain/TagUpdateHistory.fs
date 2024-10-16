a662392817e61037a21f6b67d5c32832e4b71810
kamegoro <tentyo0620@icloud.com>
2024-10-16 22:30:30 +0900

    add TagUpdateHistory domain

---
+++ TaskCanvasManager/Domain/TagUpdateHistory.fs
@@ -0,0 +1,20 @@
+namespace task_canvas_tag_manager.Domain
+
+open System
+
+type タグの更新履歴番号 = タグの更新履歴番号 of Guid
+
+type タグの更新履歴の作成日時 = タグの更新履歴の作成日時 of DateTimeOffset
+
+type タグの更新履歴 =
+    { 履歴番号: タグの更新履歴番号
+      作成日時: タグの更新履歴の作成日時
+      タグ名: タグ名
+      タグ番号: タグ番号 }
+
+module タグの更新履歴 =
+    let タグの更新履歴の作成 (タグ: タグ) : タグの更新履歴 =
+        { 履歴番号 = タグの更新履歴番号 (Guid.NewGuid())
+          作成日時 = タグの更新履歴の作成日時 (DateTimeOffset.Now.ToUniversalTime())
+          タグ名 = タグ.名前
+          タグ番号 = タグ.タグ番号 }
