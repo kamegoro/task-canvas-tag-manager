77dc93118033d7ffd3541d8b6c19b6d067f3e45a
kamegoro <tentyo0620@icloud.com>
2024-10-16 22:29:30 +0900

    update createTag test for tagHistory

--- TaskCanvasManager.Test/UseCase/CreateTagTest.fs
+++ TaskCanvasManager.Test/UseCase/CreateTagTest.fs
@@ -8,20 +8,34 @@ module タグの登録のテスト =
     [<Test>]
     let タグの新規登録がされる () =
         let mutable タグの登録called = 0
+        let mutable タグの更新履歴の作成called = 0
+
+        let タグ名 = タグ名 "タグ1"
+        let タグ = タグ.タグの作成 タグ名
+        let タグの更新履歴 = タグの更新履歴.タグの更新履歴の作成 タグ
 
         let deps: タグの登録.Deps =
-            { タグの登録 =
-                fun (タグ: タグ) ->
-                    async {
-                        match タグ with
-                        | { 名前 = タグ名 "タグ1" } -> タグの登録called <- タグの登録called + 1
-                        | _ -> ()
+            {
+                タグの登録 =
+                    fun (タグ: タグ) ->
+                        async {
+                            match タグ with
+                            | { 名前 = タグ名 "タグ1" } -> タグの登録called <- タグの登録called + 1
+                            | _ -> ()
 
-                        return ()
-                    } }
+                            return ()
+                        }
+                タグの更新履歴の作成 =
+                    fun (タグの更新履歴: タグの更新履歴) ->
+                        async {
+                            if タグの更新履歴 = タグの更新履歴 then
+                                タグの更新履歴の作成called <- タグの更新履歴の作成called + 1
 
-        let タグ = タグ.タグの作成 (タグ名 "タグ1")
+                            return ()
+                        }
+            }
 
-        タグの登録.実行 deps (タグ名 "タグ1") |> Async.RunSynchronously
+        タグの登録.実行 deps タグ名 |> Async.RunSynchronously
 
         タグの登録called |> should equal 1
+        タグの更新履歴の作成called |> should equal 1
