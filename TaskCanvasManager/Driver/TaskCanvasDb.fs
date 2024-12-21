35a68f19996939f5fefd2df7118b650329ae3b8e
kamegoro <tentyo0620@icloud.com>
2024-12-22 00:07:33 +0900

    delete async compitation for driver

--- TaskCanvasManager/Driver/TaskCanvasDb.fs
+++ TaskCanvasManager/Driver/TaskCanvasDb.fs
@@ -2,6 +2,7 @@ namespace task_canvas_tag_manager.Driver
 
 open Dapper.FSharp.PostgreSQL
 open System.Data
+open FSharpPlus
 open Dapper
 open System
 
@@ -21,59 +22,47 @@ module TaskCanvasDb =
     let tagHistoryTable = table'<TagHistory> "tag_history" |> inSchema "task_canvas"
 
     let selectTags (conn: IDbConnection) : Async<Tag list> =
-        async {
-            let! queryResult =
-                select {
-                    for t in tagTable do
-                        selectAll
-                }
-                |> conn.SelectAsync<Tag>
-                |> Async.AwaitTask
+        let queryResult =
+            select {
+                for t in tagTable do
+                    selectAll
+            }
 
-            return queryResult |> List.ofSeq
-        }
+        queryResult |> conn.SelectAsync<Tag> |> Async.AwaitTask |>> Seq.toList
 
     let selectTagById (conn: IDbConnection) (タグID: Guid) : Async<Tag> =
-        async {
-            let! queryResult =
-                select {
-                    for t in tagTable do
-                        where (t.id = タグID)
-                }
-                |> conn.SelectAsync<Tag>
-                |> Async.AwaitTask
+        let queryResult =
+            select {
+                for t in tagTable do
+                    where (t.id = タグID)
+            }
 
-            return queryResult |> Seq.toList |> List.head
-        }
+        queryResult |> conn.SelectAsync<Tag> |> Async.AwaitTask
+        |>> Seq.toList
+        |>> List.head
 
     let insertTag (conn: IDbConnection) (タグ: Tag) : Async<unit> =
-        async {
-            return!
-                insert {
-                    into tagTable
-                    value タグ
-                }
-                |> conn.InsertAsync
-                |> Async.AwaitTask
-                |> Async.Ignore
+        insert {
+            into tagTable
+            value タグ
         }
+        |> conn.InsertAsync
+        |> Async.AwaitTask
+        |> Async.Ignore
 
     let updateTag (conn: IDbConnection) (タグ: Tag) : Async<unit> =
-        async {
-            return!
-                update {
-                    for t in tagTable do
-                        set
-                            { id = タグ.id
-                              name = タグ.name
-                              is_deleted = タグ.is_deleted }
+        update {
+            for t in tagTable do
+                set
+                    { id = タグ.id
+                      name = タグ.name
+                      is_deleted = タグ.is_deleted }
 
-                        where (t.id = タグ.id)
-                }
-                |> conn.UpdateAsync
-                |> Async.AwaitTask
-                |> Async.Ignore
+                where (t.id = タグ.id)
         }
+        |> conn.UpdateAsync
+        |> Async.AwaitTask
+        |> Async.Ignore
 
     let searchTags (conn: IDbConnection) (name: string) : Async<Result<Tag list, exn>> =
         async {
@@ -89,13 +78,10 @@ module TaskCanvasDb =
         }
 
     let insertTagHistory (conn: IDbConnection) (tagHistory: TagHistory) : Async<unit> =
-        async {
-            return!
-                insert {
-                    into tagHistoryTable
-                    value tagHistory
-                }
-                |> conn.InsertAsync
-                |> Async.AwaitTask
-                |> Async.Ignore
+        insert {
+            into tagHistoryTable
+            value tagHistory
         }
+        |> conn.InsertAsync
+        |> Async.AwaitTask
+        |> Async.Ignore
