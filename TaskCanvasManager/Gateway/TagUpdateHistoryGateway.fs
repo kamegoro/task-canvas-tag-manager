d479791324641d815b4fa25e4e6c14db64b2607e
kamegoro <tentyo0620@icloud.com>
2024-12-21 23:55:13 +0900

    update tagUpdateHistoryGateway

--- TaskCanvasManager/Gateway/TagUpdateHistoryGateway.fs
+++ TaskCanvasManager/Gateway/TagUpdateHistoryGateway.fs
@@ -7,12 +7,10 @@ open System.Data
 module TagUpdateHistoryGateway =
     let タグの更新履歴の作成 (conn: IDbConnection) : タグの更新履歴.Port.タグの更新履歴の登録 =
         fun (タグの更新履歴: タグの更新履歴) ->
-            async {
-                let データベースのタグの更新履歴: TaskCanvasDb.TagHistory =
-                    { id = タグの更新履歴.履歴番号 |> fun (タグの更新履歴番号 v) -> v
-                      tag_id = タグの更新履歴.タグ番号 |> fun (タグ番号 v) -> v
-                      name = タグの更新履歴.タグ名 |> fun (タグ名 v) -> v
-                      created_at = タグの更新履歴.作成日時 |> fun (タグの更新履歴の作成日時 v) -> v }
+            let データベースのタグの更新履歴: TaskCanvasDb.TagHistory =
+                { id = タグの更新履歴.履歴番号 |> fun (タグの更新履歴番号 v) -> v
+                  tag_id = タグの更新履歴.タグ番号 |> fun (タグ番号 v) -> v
+                  name = タグの更新履歴.タグ名 |> fun (タグ名 v) -> v
+                  created_at = タグの更新履歴.作成日時 |> fun (タグの更新履歴の作成日時 v) -> v }
 
-                TaskCanvasDb.insertTagHistory conn データベースのタグの更新履歴 |> Async.RunSynchronously
-            }
+            TaskCanvasDb.insertTagHistory conn データベースのタグの更新履歴
