f2b731ee14c860c9b51a652b031ceaf6618174bb
kamegoro <tentyo0620@icloud.com>
2024-12-21 23:19:22 +0900

    delete async compitation

--- TaskCanvasManager/Gateway/TagGateway.fs
+++ TaskCanvasManager/Gateway/TagGateway.fs
@@ -1,5 +1,6 @@
 namespace task_canvas_tag_manager.Gateway
 
+open FSharpPlus
 open task_canvas_tag_manager.Driver
 open task_canvas_tag_manager.Domain
 open System.Data
@@ -8,63 +9,47 @@ open System
 module TagGateway =
     let 全てのタグの取得 (conn: IDbConnection) : タグ.Port.全てのタグの取得 =
         fun () ->
-            async {
-                let! データベースのタグ一覧 = TaskCanvasDb.selectTags conn
+            let データベースのタグ一覧 = TaskCanvasDb.selectTags conn
 
-                let タグ一覧 =
-                    データベースのタグ一覧
-                    |> List.filter (fun データベースのタグ -> データベースのタグ.is_deleted = false)
-                    |> List.map (fun タグ -> { タグ番号 = タグ番号 タグ.id; 名前 = タグ名 タグ.name })
-
-                return タグ一覧
-            }
+            データベースのタグ一覧
+            |>> List.filter (fun データベースのタグ -> データベースのタグ.is_deleted = false)
+            |>> List.map (fun データベースのタグ ->
+                { タグ番号 = データベースのタグ.id |> fun v -> タグ番号 v
+                  名前 = データベースのタグ.name |> fun v -> タグ名 v })
 
     let タグの登録 (conn: IDbConnection) : タグ.Port.タグの登録 =
         fun (タグ: タグ) ->
-            async {
-                let データベースのタグ: TaskCanvasDb.Tag =
-                    { id = タグ.タグ番号 |> fun (タグ番号 v) -> v
-                      name = タグ.名前 |> fun (タグ名 v) -> v
-                      is_deleted = false }
+            let データベースのタグ: TaskCanvasDb.Tag =
+                { id = タグ.タグ番号 |> fun (タグ番号 v) -> v
+                  name = タグ.名前 |> fun (タグ名 v) -> v
+                  is_deleted = false }
 
-                return! TaskCanvasDb.insertTag conn データベースのタグ
-            }
+            データベースのタグ |> TaskCanvasDb.insertTag conn
 
     let タグの更新 (conn: IDbConnection) : タグ.Port.タグの更新 =
         fun (タグ: タグ) ->
-            async {
-                let データベースのタグ: TaskCanvasDb.Tag =
-                    { id = タグ.タグ番号 |> fun (タグ番号 v) -> v
-                      name = タグ.名前 |> fun (タグ名 v) -> v
-                      is_deleted = false }
+            let データベースのタグ: TaskCanvasDb.Tag =
+                { id = タグ.タグ番号 |> fun (タグ番号 v) -> v
+                  name = タグ.名前 |> fun (タグ名 v) -> v
+                  is_deleted = false }
 
-                return! TaskCanvasDb.updateTag conn データベースのタグ
-            }
+            データベースのタグ |> TaskCanvasDb.updateTag conn
 
     let タグの削除 (conn: IDbConnection) : タグ.Port.タグの削除 =
         fun (タグ番号': タグ番号) ->
-            async {
-                let データベースのタグ番号: Guid = タグ番号' |> fun (タグ番号 v) -> v
+            let データベースのタグ番号: Guid = タグ番号' |> fun (タグ番号 v) -> v
 
-                let! データベースのタグ = TaskCanvasDb.selectTagById conn データベースのタグ番号
-
-                return! TaskCanvasDb.updateTag conn { データベースのタグ with is_deleted = true }
-            }
+            TaskCanvasDb.selectTagById conn データベースのタグ番号
+            |>> (fun (データベースのタグ: TaskCanvasDb.Tag) -> { データベースのタグ with is_deleted = true })
+            >>= TaskCanvasDb.updateTag conn
 
     let タグの検索 (conn: IDbConnection) : タグ.Port.タグの検索 =
         fun (タグ名': タグ名) ->
-            async {
-                let データベースのタグの変換 (タグ: TaskCanvasDb.Tag) =
-                    { タグ番号 = タグ.id |> fun v -> タグ番号 v
-                      名前 = タグ.name |> fun v -> タグ名 v }
+            let データベースのタグの変換 (タグ: TaskCanvasDb.Tag) =
+                { タグ番号 = タグ.id |> fun v -> タグ番号 v
+                  名前 = タグ.name |> fun v -> タグ名 v }
 
-                let! データベースのタグの検索結果 = TaskCanvasDb.searchTags conn (タグ名' |> fun (タグ名 v) -> v)
-
-                match データベースのタグの検索結果 with
-                | Ok タグリスト ->
-                    let タグ一覧 = タグリスト |> List.map データベースのタグの変換
-                    return タグ一覧
-                | Error ex ->
-                    // Handle the error case appropriately
-                    return raise ex
-            }
+            TaskCanvasDb.searchTags conn (タグ名' |> fun (タグ名 v) -> v)
+            |>> (function
+            | Ok タグリスト -> タグリスト |> List.map データベースのタグの変換
+            | Error ex -> raise ex)
