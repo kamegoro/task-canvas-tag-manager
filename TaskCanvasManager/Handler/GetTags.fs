20bd67bd91236f8cb08bc1db143e51e22b7cbffa
kamegoro <tentyo0620@icloud.com>
2024-12-25 00:00:20 +0900

    fix handler

--- TaskCanvasManager/Handler/GetTags.fs
+++ TaskCanvasManager/Handler/GetTags.fs
@@ -10,11 +10,12 @@ module GetTags =
     type Tag = { id: string; name: string }
     type ResponseJson = { tags: Tag list }
 
-    let toJson (tags: タグ list) =
+    let toJson (tags: タグ list) : ResponseJson =
         tags
         |> List.map (fun tag ->
             { id = tag.タグ番号 |> fun (タグ番号 v) -> v.ToString()
               name = tag.名前 |> fun (タグ名 v) -> v })
+        |> fun tags -> { tags = tags }
 
     let handler (deps: 全てのタグの取得.Deps) : Async<IResult> =
         全てのタグの取得.実行 deps |> Async.map toJson |> Async.map (fun json -> Results.Ok json)
