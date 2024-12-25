bc4b002485ebacd9812bb5f7aa927f8d8fb368c9
kamegoro <tentyo0620@icloud.com>
2024-12-25 10:27:35 +0900

    update createTag useCase

--- TaskCanvasManager/UseCase/CreteTag.fs
+++ TaskCanvasManager/UseCase/CreteTag.fs
@@ -1,6 +1,7 @@
 namespace task_canvas_tag_manager.UseCase
 
 open task_canvas_tag_manager.Domain
+open FSharpPlus
 
 module タグの登録 =
     type Deps =
@@ -10,9 +11,9 @@ module タグの登録 =
     let 実行 (deps: Deps) (名前: タグ名) : Async<unit> =
         let タグ' = タグ.タグの作成 名前
 
-        let タグの登録 (タグ': タグ) : タグ = タグ.登録 deps.タグの登録 タグ' |> fun _ -> タグ'
+        let タグの登録 (タグ'': タグ) : Async<タグ> = タグ.登録 deps.タグの登録 タグ'' |>> fun _ -> タグ''
 
-        let タグの更新履歴の登録 (タグ': タグ) : Async<unit> =
-            タグの更新履歴.タグの更新履歴の作成 タグ' |> タグの更新履歴.登録 deps.タグの更新履歴の作成
+        let タグの更新履歴の登録 (タグ'': タグ) : Async<unit> =
+            タグの更新履歴.タグの更新履歴の作成 タグ'' |> タグの更新履歴.登録 deps.タグの更新履歴の作成
 
-        タグ' |> タグの登録 |> タグの更新履歴の登録
+        タグ' |> タグの登録 >>= タグの更新履歴の登録
